c1d9a261f904d0d0bc88d7ebd3e0478ee553ebd3
hasan rami aydin <72613302+hasanraydin@users.noreply.github.com>
2026-02-09 10:36:47 +0100

    Update DRT parameters in IDFModeParameters.java

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/mode_choice/parameters/IDFModeParameters.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/mode_choice/parameters/IDFModeParameters.java
@@ -37,9 +37,9 @@ public class IDFModeParameters extends ModeParameters {
 
 		// Drt
 		parameters.drt.alpha_u = 0.0;								//base value = 0.0
-		parameters.drt.betaTravelTime_u_min = 0.2;					//base value = -0.2
-		parameters.drt.betaWaitingTime_u_min = 0.1;				//base value = -0.1
-		parameters.drt.betaAccessEgressTime_u_min = 0.1;		//base value = -0.0804
+		parameters.drt.betaTravelTime_u_min = 1.2;					//base value = -0.2
+		parameters.drt.betaWaitingTime_u_min = -0.1;				//base value = -0.1
+		parameters.drt.betaAccessEgressTime_u_min = -0.0804;		//base value = -0.0804
 
 		// PT
 		parameters.pt.alpha_u = 0.0;								//eqasim-bavaria-main value
